8e21ec7ee9d0ab53f1f3d7c0a708f65a1cac556d
Aneesha0 <aneeshambail5@gmail.com>
2024-06-12 21:04:51 +0530

    changed allowed host setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kgzmppy=p6rk@fjnsnsb92+3zzf9jnzv%ye2pdcb2!9j167!b2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite','localhost','192.168.1.5']
 
 
 # Application definition
